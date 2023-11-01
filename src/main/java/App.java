673de00f82e318af4dee2b90eca1f08df364d617
AnastasiiaTurbal <turbal@list.ru>
2023-11-01 21:21:24 +0300

    Создан bean Cat, пройдены Тесты

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,21 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == bean2);
+        System.out.println(beanCat1 == beanCat2);
+
     }
 }
+
+
+
+        //5. В классе App, по аналогии, вызовите еще раз бин HelloWorld, затем 2 раза вызовите бин cat. Сравните 2 пары бинов по ссылке и выведите результат в консоль. Для пары HelloWorld должно вернуться true, для cat - false. Так вы можете увидеть результат того, как работает наш контейнер.
+        //6. Раскомментируйте тестовый класс и проверьте своё решение.
